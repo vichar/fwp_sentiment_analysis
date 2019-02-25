811249dc1e2736248e5ac43e2e331c3c029c54d0
Vichar Nuchsiri 52545 <vichar.nuchsiri@fico.com>
2019-02-26 00:07:37 +0700

    first attemp at API post

--- main.go
+++ main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"github.com/labstack/echo"
-	"github.com/vichar/fizzbuzz"
+	"github.com/vichar/rssfeed"
 )
 
 func main() {
+
+	channels := make([]rssfeed.RSSChannel, 0)
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.GET("/fizzbuzz/:number", func(c echo.Context) error {
-		number := c.Param("number")
-		n, error := strconv.Atoi(number)
+	e.POST("/programs", func(c echo.Context) error {
+		feed := c.FormValue("rssURL")
+		response, error := rssfeed.HTTPGet(feed)
 		if error != nil {
-			return c.JSON(http.StatusBadRequest, map[string]string{
-				"message": error.Error(),
-			})
+			e.Logger.Error("HTTP Client Error")
+			return c.JSON(http.StatusNotAcceptable, "Error Parsing HTTP Response")
 		}
-		if n > 5 {
-			return c.JSON(http.StatusBadRequest, map[string]string{
-				"message": number + " is not a supported number.",
-			})
+		httpResponse := rssfeed.ParseHTTPResponse(response)
+		channel, parseError := rssfeed.ParseRSSData(httpResponse.Body)
+		if parseError != nil {
+			e.Logger.Error("Error Parsing HTTP Response")
+			return c.JSON(http.StatusNotAcceptable, "Error Parsing HTTP Response")
 		}
-		return c.JSON(http.StatusOK, map[string]string{
-			"message": fizzbuzz.Say(n),
-		})
+		channels = append(channels, channel)
+		channelsJSON, jsonError := json.Marshal(channels)
+		if jsonError != nil {
+			e.Logger.Error("Error Parsing Data")
+			return c.JSON(http.StatusNotAcceptable, "Error Parsing Data")
+		}
+
+		return c.JSON(http.StatusOK, string(channelsJSON))
+
 	})
+	// e.GET("/fizzbuzz/:number", func(c echo.Context) error {
+	// 	number := c.Param("number")
+	// 	n, error := strconv.Atoi(number)
+	// 	if error != nil {
+	// 		return c.JSON(http.StatusBadRequest, map[string]string{
+	// 			"message": error.Error(),
+	// 		})
+	// 	}
+	// 	if n > 5 {
+	// 		return c.JSON(http.StatusBadRequest, map[string]string{
+	// 			"message": number + " is not a supported number.",
+	// 		})
+	// 	}
+	// 	return c.JSON(http.StatusOK, map[string]string{
+	// 		"message": fizzbuzz.Say(n),
+	// 	})
+	// })
 	e.Logger.Fatal(e.Start(":1323"))
 }
